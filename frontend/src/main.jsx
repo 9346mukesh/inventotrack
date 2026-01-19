ce922bbe17b5099426703c1bb0cd64bc5aaaceaa
9346mukesh <9346mukeshkumarreddy@gmail.com>
2026-01-19 11:06:31 +0530

    Handle prod API url and stripe env keys

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,10 @@ import { CartProvider } from "./context/CartContext";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+const stripePromise = loadStripe(
+  import.meta.env.VITE_STRIPE_PUBLIC_KEY ||
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
